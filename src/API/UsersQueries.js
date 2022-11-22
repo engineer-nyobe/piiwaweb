fcab07b4ad4c1372423bfce7c49346b1873f5694
NYOBE Hubert <hnyobe0@gmail.com>
2022-11-22 06:35:21 +0100

    state of the last commit of 21/11/22

--- src/API/UsersQueries.js
+++ src/API/UsersQueries.js
@@ -4,7 +4,7 @@ import { API_URL } from "./Config";
 export const GetAllUsers = async () => {
   return axios.get(`${API_URL}/users`).then((res) => res.data);
 };
-export const GetUserById = async ({id}) => {
+export const GetUserById = async (id) => {
   return axios.get(`${API_URL}/users/${id}`).then((res) => res.data);
 };
 export const CountUsers = async () => {
--- src/VIEWS/PAGES/UserInfos.jsx
+++ src/VIEWS/PAGES/UserInfos.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { GetUserById } from "../../API/UsersQueries";
+import {
+  Grid,
+  TextField,
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardMedia,
+} from "@mui/material";
+import TopBar from "../COMPONENTS/TopBar";
 
 const UserInfos = () => {
-  return (
-    <div>UserInfos</div>
-  )
-}
+  const { id } = useParams();
 
-export default UserInfos
+  const [UserData, setUserData] = useState({});
+
+  const getUserInfo = async () => {
+    const data = await GetUserById(id);
+    setUserData(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  return (
+    <div>
+      <TopBar />
+      {UserData ? (
+        <Grid container spacing={2}>
+          <Grid item sm={6}>
+            <Card sx={{ maxWidth: 500 }}>
+              <CardMedia
+                component="img"
+                height="500"
+                image={UserData.userqrcode}
+                alt="green iguana"
+              />
+              <CardContent>
+                <Box
+                  component="form"
+                  sx={{
+                    "& > :not(style)": { m: 1, width: "25ch" },
+                  }}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <TextField
+                    id="outlined-basic"
+                    label="User Phone"
+                    variant="outlined"
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                    value={UserData.phone}
+                  />
+                  <TextField
+                    id="outlined-basic"
+                    label="User full name"
+                    variant="outlined"
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                    value={UserData.username}
+                  />
+                  <TextField
+                    id="outlined-basic"
+                    label="User role"
+                    variant="outlined"
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                    value={UserData.role}
+                  />
+                  <TextField
+                    id="outlined-basic"
+                    label="User company"
+                    variant="outlined"
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                    value={UserData.company}
+                  />
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item sm={6}>
+            <Card sx={{ maxWidth: 700 }}>
+              <CardContent>
+                <Typography>{UserData._id} </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      ) : (
+        <div>
+          <h2>NO USER FOUND</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserInfos;
